5aee72341f8c0d44c0fd58cba6eb660c27669894
qiyonghan <2502510147.com>
2023-05-21 16:38:24 +0800

    把所有参与的配置删除，把所需要的python包打包发上去了

--- bot.bat
+++ bot.bat
@@ -1,3 +1,5 @@
 title dayzMiraiBot
 cd dayzOnlineNumber
+set python = "./python/python.exe"
 python miraiBot.py
+pause
---
+++ python/Scripts/qiniupy-script.py
@@ -0,0 +1,12 @@
+#!"c:\program files\python\python.exe"
+# EASY-INSTALL-ENTRY-SCRIPT: 'qiniu==7.2.8','console_scripts','qiniupy'
+__requires__ = 'qiniu==7.2.8'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('qiniu==7.2.8', 'console_scripts', 'qiniupy')()
+    )
